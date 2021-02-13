33964fd3d384e5685b6330829fde14e01a37812c
Facundo Lanus <facundolanus@gmail.com>
2021-02-13 16:51:28 -0300

    se hizo el navbar para el home page, la gome page con el banner, explorar nuestras categorias, proyectos destacados y las cards de ultimos proyectos

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
   has_many :category_projects
   validates :title, presence: true
-  validates :title, inclusion: { in: ["diseño gráfico",  "fotografía", "video", "ilustración", "diseño ux/ui responsive"),
-    message: "%{value} no es una categoria valida" }
+  validates :title, presence: true
 end
