135cafee9436c5cda07f92cb8efd21bf62a317c1
camilo gonzalez <camilogzlez@gmail.com>
2021-02-16 20:40:32 -0300

    postulations controller done

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,11 +1,13 @@
 import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"
 
 
 const initflatpickr = () => {
   flatpickr(".datepicker", {
     enableTime: true,
     dateFormat: "Y-m-d H:i",
+    locale: 'es',
   });
-}
+};
 
 export { initflatpickr };
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :projects, only: [:index, :show, :new, :create, :edit, :update]
+  resources :projects, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :postulations, only: [:index, :show, :new, :create]
+  end
   get '/project/:id', to: 'projects#unpublish', as: 'unpublish'
+  patch '/project/:id/postulations/accept', to: 'postulations#accept_project', as: 'accept_project'
+  patch '/project/:id/postulations/decline', to: 'postulations#decline_project', as: 'decline_project'
 end
