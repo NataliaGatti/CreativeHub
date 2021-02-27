9209ff2b59d57d3108cd3a75e093c8825474834d
Natalia Gatti <gattinati.ng@gmail.com>
2021-02-27 18:40:03 -0300

    Improve the frontend of the project´s show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ import "bootstrap";
 import { initflatpickr } from "../plugins/init_flatpickr";
 import { initSweetalert } from '../plugins/init_sweetalert';
 import { initSelect2 } from '../plugins/init_select2';
+// import { init } from '../plugins/prueba';
 // import "../plugins/init_flatpickr";
 
 // Internal imports, e.g:
@@ -31,6 +32,7 @@ import { initSelect2 } from '../plugins/init_select2';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  
    initflatpickr();
   initSelect2();
    initSweetalert('#sweet-alert-demo', {
